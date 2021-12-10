f9894fb2ce9f0fbfb1faaed0bc589bdff9e75587
Anup Bhandari <anupbh57gmail.com>
2021-12-11 03:33:44 +1100

    Sidebar Designed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./man.png";
+const userName = "Anup Bhandari"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main container-fluid">
+      <div className="row main-wrapper">
+        <div className="sidebar col-2 container-fluid">
+          <div className="sidebar-wrapper row">
+            <div className="logo-container col-12">
+              <img src={logo} className="logo"></img>
+            </div>
+            <div className="sidebar-content col-12">
+              <div className="s-wrapper">
+                <ul className="s-list">
+                  <li className="s-li">Dashboard</li>
+                  <li className="s-li">Account</li>
+                  <li className="s-li">Schedule</li>
+                  <li className="s-li">About</li>
+                  <li className="s-li">Contact</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="content col-10">
+          <nav className="nav">
+            <div className="nav-wrapper d-flex justify-content-between">
+              <div className="nav-profile d-flex">
+                <img src = {logo} className = "usr-logo"></img>
+                <h3 class = "usr-name">{userName}</h3>
+              </div>
+              <div className="nav-logo">
+                
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
     </div>
   );
 }
