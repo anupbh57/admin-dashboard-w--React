55c26cfa910f81e6344dfbfc53465b053f0c8d4b
Anup Bhandari <anupbh57gmail.com>
2021-12-11 15:43:32 +1100

    reformed navigation bar and changed layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import React from "react";
 import logo from "./man.png";
-const userName = "Anup Bhandari"
+import ReactDOM from "react-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faCog } from "@fortawesome/free-solid-svg-icons";
+import { faPowerOff } from "@fortawesome/free-solid-svg-icons";
+const arrowDown = <FontAwesomeIcon icon={faChevronDown} />
+const envelope = <FontAwesomeIcon icon={faEnvelope} />;
+const cog = <FontAwesomeIcon icon={faCog} />;
+const powerOff = <FontAwesomeIcon icon={faPowerOff} />;
+
+const userName = "Anup Bhandari";
+const notifNumber = 12;
 function App() {
   return (
-    <div className="main container-fluid">
-      <div className="row main-wrapper">
-        <div className="sidebar col-2 container-fluid">
+    <div className="main">
+      <nav className="nav">
+        <div className="nav-wrapper d-flex justify-content-between">
+          <div className="nav-logo">LOGO</div>
+          <div className="nav-right d-flex justify-content-between">
+            <div className="nav-search input-group input-group-sm">
+              <input
+                type="text"
+                className="form-control"
+                aria-label="Small"
+                aria-describedby="inputGroup-sizing-sm"
+                placeholder="Search"
+              ></input>
+            </div>
+            <div className="nav-profile d-flex justify-content-center">
+              <div className="nav-profile-left d-flex justify-content-center">
+                <img src={logo} className="usr-logo"></img>
+
+                <h3 class="usr-name">{userName}</h3>
+                <div className="notif">{notifNumber}</div>
+                <div className="usr-arrow">{arrowDown}</div>
+              </div>
+            </div>
+            <div className="nav-controls d-flex justify-content-between">
+              {cog}
+              {powerOff}
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <div className="d-flex w-100 main-wrapper">
+        <div className="sidebar w-30 container-fluid">
           <div className="sidebar-wrapper row">
             <div className="logo-container col-12">
-              <img src={logo} className="logo"></img>
+              <h2>LOGO</h2>
             </div>
             <div className="sidebar-content col-12">
               <div className="s-wrapper">
@@ -23,19 +65,7 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="content col-10">
-          <nav className="nav">
-            <div className="nav-wrapper d-flex justify-content-between">
-              <div className="nav-profile d-flex">
-                <img src = {logo} className = "usr-logo"></img>
-                <h3 class = "usr-name">{userName}</h3>
-              </div>
-              <div className="nav-logo">
-                
-              </div>
-            </div>
-          </nav>
-        </div>
+        <div className="content w-70"></div>
       </div>
     </div>
   );
